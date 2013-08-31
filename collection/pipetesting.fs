a96e77659c5ddacc28f7934ed9242651dddc979f
harrypower <philipkingsmith@gmail.com>
2013-08-31 08:13:00 +0000

    testing words made and tested for pipe vs file method

--- collection/pipetesting.fs
+++ collection/pipetesting.fs
@@ -22,3 +22,61 @@ variable junk$
     endtry ;
 
 
+: time_test
+    utime
+    makepipe
+    s" /home/pi/git/datalogger/gpio/cadmium-sensor.fs 7" read-sensor throw 2drop \ type cr
+    close-mypipe
+    utime 2swap d- 
+;
+
+: dotime-test
+    0 0 50 0 ?do time_test d+ loop 2dup d. 1 51 m*/ d. ;
+
+: time-test1
+    utime
+    s" /home/pi/git/datalogger/gpio/cadmium-sensor.fs 7" read-sensor throw 2drop \ type cr
+    utime 2swap d- ;
+
+: dotime-test1
+    makepipe
+    0 0 50 0 ?do time-test1 d+ loop 2dup d. 1 51 m*/ d.
+    close-mypipe ;
+
+: maketemp
+    s" sudo touch /var/tmp/gforth-tmp-file" system $? throw
+    s" sudo chmod 666 /var/tmp/gforth-tmp-file" system $? throw ;
+
+
+: close-temp
+    s" sudo rm /var/tmp/gforth-tmp-file" system $? throw ;
+
+: read-sensor-file ( caddr u -- caddr1 u1 nflag ) \ nflag is zero for sensor crrectly read.
+    try
+	{ caddr u }
+	s" sudo " junk$ $! caddr u junk$ $+! s"  > /var/tmp/gforth-tmp-file" junk$ $+! junk$ $@ system $? throw
+	s" /var/tmp/gforth-tmp-file" slurp-file
+	false
+    restore
+    endtry ;
+	
+    
+: time-test2
+    utime
+    maketemp
+    s" /home/pi/git/datalogger/gpio/cadmium-sensor.fs 7" read-sensor-file throw 2drop \ type cr
+    close-temp
+    utime 2swap d- ;
+
+: dotime-test2
+    0 0 50 0 ?do time-test2 d+ loop 2dup d. 1 51 m*/ d. ;
+
+: time-test3
+    utime
+    s" /home/pi/git/datalogger/gpio/cadmium-sensor.fs 7" read-sensor-file throw 2drop \ type cr
+    utime 2swap d- ;
+
+: dotime-test3
+    maketemp
+    0 0 50 0 ?do time-test3 d+ loop 2dup d. 1 51 m*/ d. 
+    close-temp ;
