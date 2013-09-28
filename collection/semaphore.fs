10f3346b66eab2fbd9c0ce367e0a78dd8f852f5f
harrypower <philipkingsmith@gmail.com>
2013-09-28 02:32:39 +0000

    Changed compiler directive test to be a colon defined word semaphore-constants instead of the shared library definded word sem-info-sem_t

--- collection/semaphore.fs
+++ collection/semaphore.fs
@@ -14,7 +14,7 @@
 \    You should have received a copy of the GNU General Public License
 \    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-[IFUNDEF] sem-info-sem_t
+[IFUNDEF] semaphore-constants
 
 clear-libs
 c-library mysemaphore
