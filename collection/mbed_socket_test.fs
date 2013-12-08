1a950cb417a329337cfd468300743831c6bb233e
harrypower <philipkingsmith@gmail.com>
2013-12-08 23:18:13 +0000

    More study of the mbed socket connection stuff!

--- collection/mbed_socket_test.fs
+++ collection/mbed_socket_test.fs
@@ -14,12 +14,18 @@ here to buffer 500 allot
 : mbedread-client ( -- caddr u )
     s" Start client!" type cr
     mbed-ip$ $@ mbed-port# open-socket { socketid }
-    s\" GET / \r\n\r\n" socketid write-socket
+    s\" GET /read \r\n\r\n" socketid write-socket
     0 0 begin
     	2drop
-	socketid buffer 499 read-socket-from
-    	dup 1 >
+	socketid buffer 499 read-socket  \ note read-socket is for TCP read-socket-from is for UDP
+    	dup 10 >
     until
     socketid close-socket ;
 
+: readmany ( -- )
+    10 0 ?do
+	mbedread-client type cr
+	4000 ms \ wait 4 seconds
+    loop
+;
 
