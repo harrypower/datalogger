9dbdcc3015e4064c7a8840f00863eb80d3f04bdf
root <root@rpi1.(none)>
2013-12-11 08:19:14 +0000

    Playing around with ideas here sqlite3 test with a c++ interface like program just wanted to keep here for posterity Will probably not use this type of interface or sqlite3 anyways!

--- collection/mbed_socket_test.fs
+++ collection/mbed_socket_test.fs
@@ -11,21 +11,28 @@ s" 192.168.0.116" mbed-ip$ $!
 here to buffer 500 allot
 
 
-: mbedread-client ( -- caddr u )
-    s" Start client!" type cr
-    mbed-ip$ $@ mbed-port# open-socket { socketid }
+: mbedread-client ( caddr u nport# -- caddr1 u1 )
+    \ s" Start client!" type cr
+    open-socket { socketid }
     s\" GET /read \r\n\r\n" socketid write-socket
     0 0 begin
-    	2drop
-	socketid buffer 499 read-socket  \ note read-socket is for TCP read-socket-from is for UDP
-    	dup 10 >
+	2drop
+ 	socketid buffer 499 read-socket  \ note read-socket is for TCP read-socket-from is for UDP
+	2dup s\" \r\n\r\n" search swap drop swap drop
+	\ this will bail if it finds end cr lf cr lf or it will generate error 
     until
     socketid close-socket ;
 
-: readmany ( -- )
+: readmany (  -- )
     10 0 ?do
-	mbedread-client type cr
+	mbed-ip$ $@ mbed-port# mbedread-client  ." total from mbed: " dup . cr type cr
 	4000 ms \ wait 4 seconds
     loop
 ;
+
+: read120 ( -- )
+    s" 192.168.0.120" 4446 mbedread-client ." total: " dup . cr cr type cr ;
+
+: read116 ( -- )
+    s" 192.168.0.116" 4446 mbedread-client ." total: " dup . cr cr type cr ;
 
