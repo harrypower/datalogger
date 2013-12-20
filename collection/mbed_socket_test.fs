691d635b57e4769cd10f2ce23e8bd109b80e941d
harrypower <philipkingsmith@gmail.com>
2013-12-20 08:03:18 +0000

    Cluggy but it does let me test ideas out.  I can log data from the mbed over a socket now Loads of stuff need to be done to get this in any shape for real use but this stuff will allow playing

--- collection/mbed_socket_test.fs
+++ collection/mbed_socket_test.fs
@@ -3,6 +3,7 @@
 include ../string.fs
 include ../socket.fs
 include ../Gforth-Tools/sqlite3_gforth_lib.fs
+include errorlogging.fs
 
 decimal 
 4446 value mbed-port#
@@ -11,7 +12,16 @@ variable mbed-ip$
 s" 192.168.0.116" mbed-ip$ $!
 0 value buffer
 here to buffer 500 allot
+variable junk$ junk$ $init
+variable temp$ temp$ $init
 
+
+
+: #to$ ( n -- c-addr u1 ) \ convert n to string then add a "," at the end of the converted string
+    s>d
+    swap over dabs
+    <<# #s rot sign #> #>>
+        junk$ $! s" ," junk$ $+! junk$ $@ ;
 
 : mbedread-client ( caddr u nport# -- caddr1 u1 )
     \ s" Start client!" type cr
@@ -56,10 +66,44 @@ here to buffer 500 allot
     restore dup if 0 swap 0 swap then
     endtry ;
 
-: ck-thp$ ( caddr u count -- nflag )  \ looks at string to see if all data is present by finding 4 comma's 
+: ck-thp$ ( caddr u -- nflag )  \ looks at string to see if all data is present by finding 4 comma's 
     0 { count }  \ if 4 comma's are found then false is returned true is returned if any other value is found
     0 ?do
 	dup c@ ',' = if count 1+ to count then 1+
     loop drop
     count 4 = if false else true then ;
+
+
+: !data ( caddr u -- )
+    s" atest.data" dbname
+    s" insert into thpdata values(" temp$ $!
+    datetime datetime$ $@ temp$ $+!
+    temp$ $+!
+    s" );" temp$ $+!
+    temp$ $@ dbcmds
+    sendsqlite3cmd throw
+;
+
+: gcs-thps$ ( -- )
+    get-thp$ 0 =
+    if
+	{ caddr u } caddr u
+	ck-thp$ 0 =
+	if  \ data from mbed checks out now put in database
+	    caddr u !data
+	then
+    else
+	2drop \ this is some error so maybe store that!
+    then ;
+
+: createdb ( -- )
+    s" atest.data" dbname
+    s" CREATE TABLE IF NOT EXISTS thpdata(year int,month int,day int, hour int, min int, sec int, age int,temp int,humd int,btemp int, pressure int);" dbcmds
+    sendsqlite3cmd throw ;
+
+: see-db ( -- caddr u )
+    s" atest.data" dbname
+    s" select * from thpdata;" dbcmds
+    sendsqlite3cmd throw
+    dbret$ ;
 
