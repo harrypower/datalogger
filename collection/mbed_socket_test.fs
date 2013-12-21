aeb8f5901f6a9067e2814a3b759fd6a473f3db73
harrypower <philipkingsmith@gmail.com>
2013-12-20 20:45:18 -0700

    update project with Gforth-Tools bug fix and first working logging test program

--- collection/mbed_socket_test.fs
+++ collection/mbed_socket_test.fs
@@ -8,16 +8,27 @@ include errorlogging.fs
 decimal 
 4446 value mbed-port#
 
-variable mbed-ip$
-s" 192.168.0.116" mbed-ip$ $!
 0 value buffer
 here to buffer 500 allot
 variable junk$ junk$ $init
 variable temp$ temp$ $init
 
 
+struct
+    cell% field http$
+    cell% field socketterm$
+    cell% field mbed-ip$ 
+    cell% field mbed-dbname$
+end-struct strings%
+
+create mystrings% strings% %allot drop
+
+s" HTTP/1.0 200 OK" mystrings% http$ $!
+s\" \r\n\r\n" mystrings% socketterm$ $!
+s" 192.168.0.116" mystrings% mbed-ip$ $!
+s" sensordb.data" mystrings% mbed-dbname$ $!
+
 : mbedread-client ( caddr u nport# -- caddr1 u1 )
-    \ s" Start client!" type cr
     open-socket { socketid }
     s\" GET /val \r\n\r\n" socketid write-socket
     0 0 begin
@@ -28,27 +39,14 @@ variable temp$ temp$ $init
     until
     socketid close-socket ;
 
-: readmany (  -- )
-    10 0 ?do
-	mbed-ip$ $@ mbed-port# mbedread-client  ." total from mbed: " dup . cr type cr
-	4000 ms \ wait 4 seconds
-    loop
-;
-
-: read120 ( -- )
-    s" 192.168.0.120" 4446 mbedread-client ." total: " dup . cr cr type cr ;
-
-: read116 ( -- )
-    s" 192.168.0.116" 4446 mbedread-client ." total: " dup . cr cr type cr ;
-
 : get-thp$ ( -- caddr u nflag )  \ reads the mbed server and returns the data to be inserted into db
     try   \ flag is false for reading of mbed was ok true means reading failed for some reason
-	mbed-ip$ $@ mbed-port# mbedread-client
-	s" HTTP/1.0 200 OK" search
+	mystrings% mbed-ip$ $@ mbed-port# mbedread-client
+	mystrings% http$ $@ search
 	if
-	    s\" \r\n\r\n" search
+	    mystrings% socketterm$ $@ search
 	    if
-		2dup 4 + s\" \r\n\r\n" search
+		2dup 4 + mystrings% socketterm$ $@ search
 		if 2drop 8 - swap 4 + swap false else 2drop 2drop true then
 	    else
 		2drop true
@@ -68,7 +66,7 @@ variable temp$ temp$ $init
 
 
 : !data ( caddr u -- )
-    s" atest.data" dbname
+    mystrings% mbed-dbname$ $@ dbname
     s" insert into thpdata values(" temp$ $!
     datetime datetime$ $@ temp$ $+!
     temp$ $+!
@@ -77,26 +75,60 @@ variable temp$ temp$ $init
     sendsqlite3cmd throw
 ;
 
-: gcs-thps$ ( -- )
-    get-thp$ 0 =
+: gcs-thp$ ( -- )  \ get check store temperature humidity pressure string into database or throw errors
+    get-thp$ dup 0 =
     if
-	{ caddr u } caddr u
+	drop 2dup 
 	ck-thp$ 0 =
 	if  \ data from mbed checks out now put in database
-	    caddr u !data
+	    !data
+	else \ data is missing throw error
+	    2drop s" Data from mbed incomplete!" exception throw
 	then
     else
-	2drop \ this is some error so maybe store that!
+	2drop s" Socket failure in get-thp$ function" exception throw
     then ;
 
 : createdb ( -- )
-    s" atest.data" dbname
+    mystrings% mbed-dbname$ $@ dbname
     s" CREATE TABLE IF NOT EXISTS thpdata(year int,month int,day int, hour int, min int, sec int, age int,temp int,humd int,btemp int, pressure int);" dbcmds
+    sendsqlite3cmd throw
+    s" CREATE TABLE IF NOT EXISTS errors(year int,month int,day int,hour int,min int,sec int,error int);" dbcmds
     sendsqlite3cmd throw ;
 
 : see-db ( -- caddr u )
-    s" atest.data" dbname
+    mystrings% mbed-dbname$ $@ dbname
     s" select * from thpdata;" dbcmds
     sendsqlite3cmd throw
     dbret$ ;
 
+: see-dberrors ( -- caddr u )
+    mystrings% mbed-dbname$ $@ dbname
+    s" select * from errors;" dbcmds
+    sendsqlite3cmd throw
+    dbret$ ;
+
+: !error ( n -- nerror ) \ nerror is the returned error from sendsqlite3cmd false for ok anything else is an error
+    mystrings% mbed-dbname$ $@ dbname
+    s" insert into errors values(" temp$ $!
+    datetime datetime$ $@ temp$ $+!
+    #to$ 1- temp$ $+!
+    s" );" temp$ $+!
+    temp$ $@ dbcmds
+    sendsqlite3cmd  \ if sendsqlite3cmd produces and error here figure out how to handle it
+;
+
+: main_process ( -- nerror )
+    TRY
+	begin
+	    gcs-thp$
+	    30000 ms \ get the data every 30 seconds 
+	again
+    RESTORE dup if dup !error dup 0<> if !error drop else drop then then 
+    ENDTRY ;
+
+: main_loop ( -- )
+    createdb
+    begin
+	main_process -28 = if true else false then \ bail only if user canceled program
+    until ;
