9259254c8b5d6a0802a12fb516d84938af2d9d31
harrypower <philipkingsmith@gmail.com>
2014-07-25 04:18:11 +0000

    updated the buffer word changed it to bufr$: and added a recall feature

--- collection/gforth-misc-tools.fs
+++ collection/gforth-misc-tools.fs
@@ -155,15 +155,20 @@ variable mytemppad$
     create addr ,
     (list$>$!) ;         \ set doer for name string copier
 
+\ list$: is used as follows:
+\ list$: mystrings
+\ Now mystrings is a dynamic string handler
+\ mystrings will return address and index to stack.  Index is the total strings in array
+\ mystrings also resets the iterator
+\ s" some string" mystring-$! \ this is the method you used to add a string to the array
+\ mystring-$off \ this will free the strings and the pointer addresses
+\ mystring-$@ type \ this will type the current string and will iterate through them each time used
+\ list$: otherstrings
+\ mystring otherstrings->$! \ this would transfer all strings from mystring to otherstrings handler 
 \ ************************************************************************
 
-: stringbuffer: ( runtime: caddr u -- caddr1 u1 ) \ will take a string from stack then return it back to stack
-    ( compiletime: "name" -- ) 
-    \ the point of this is it acts as a fifo buffer with only one space
-    \ The main use is to put strings into new allocated memory to prevent s" use causing invalid memory errors
-    \ and free the old string memory so there is no leaks
-    create 0 , 0 ,
-  does>
+: (bufr$!)
+  does> ( caddr u -- caddr1 u1 )
     dup 2swap                         ( caddr u -- sa sa caddr u )
     dup allocate throw                ( sa sa caddr u -- sa sa caddr u anew )
     dup 2swap                         ( sa sa caddr u anew  -- sa sa anew anew caddr u )
@@ -171,6 +176,37 @@ variable mytemppad$
     rot dup @ 0 <>                    ( sa sa anew -- sa anew sa f )
     if @ free throw else drop then    ( sa anew sa -- sa anew  )
     over ! dup cell +                 ( sa anew -- sa sa+4 )
-    r> swap ! dup @ swap cell + @  ;  ( sa sa+4 -- sa@ sa+4@ )
+    r> swap ! dup @ swap cell + @ ;   ( sa sa+4 -- sa@ sa+4@ )
 
+: bufr$: ( runtime: caddr u -- caddr1 u1 ) \ will take a string from stack then return it back to stack
+    ( compiletime: "name" -- ) 
+    \ the point of this is it acts as a fifo buffer with only one space
+    \ The main use is to put strings into new allocated memory to prevent s" use causing invalid memory errors
+    \ and free the old string memory so there is no leaks
+    create here 0 , 0 ,
+    latest { addr nt }   
+    nt name>string 2 + dup allocate throw addr ! 
+    dup addr cell + !
+    addr @ swap move
+    addr cell + @ 2 - addr @ + '$' swap c!
+    addr cell + @ 1 - addr @ + '@' swap c!
+    addr @ addr cell + @ nextname
+    (bufr$!) 
+    create addr , 
+  does> @ dup @ swap cell + @ ;
 
+\ bufr$: is used as follows:
+\ bufr$: mybuffer
+\ This will create a buffer called mybuffer
+\ s" some strings" mybuffer type
+\ This takes the string from the stack and puts it into the buffer and then outputs the new address and count
+\ mybuffer$@ type \ this puts the current string in the buffer onto the stack
+\ This buffer handles all the memory allocating and freeing so just use it.
+\ The point here is when a string is made like s" some thing" it has an unknown lifetime so if you
+\ are passing this strings address and count around depending on other code that s" string may not
+\ be valid memory at some point so the buffer would prevent problems.
+\ Relize the buffer only contains the last thing it has recieved so it should always be valid
+\ but it only will contain the last thing you gave it! If you pass the address around for a string
+\ in the buffer it will only be valid until a new string is made then it gets freed so do not keep
+\ the address rather use the buffer to get the address.
+\ ************************************************************************
