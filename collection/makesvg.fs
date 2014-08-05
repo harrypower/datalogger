30c5a7946637f663cc7aa7bbc5de3304b9db6b51
harrypower <philipkingsmith@gmail.com>
2014-08-05 22:22:00 +0000

    working graph svg stuff with cirles connecting lines

--- collection/makesvg.fs
+++ collection/makesvg.fs
@@ -24,7 +24,23 @@ require string.fs
 list$: lineattrn  \ svg attributes names to work with a line
 list$: lineattrv  \ svg attribute values to work with a line but are paired with linattrn names
 
-: init-svg-attributes ( -- ) \ initalizes lineattrn to default values
+: svg-attr#1 ( -- ) \ initalizes lineattrn to default values
+    lineattrn-$off
+    s" fill="           lineattrn-$!
+    s" fill-opacity="   lineattrn-$!
+    s" stroke="         lineattrn-$!
+    s" stroke-opacity=" lineattrn-$!
+    s" stroke-width="   lineattrn-$!
+    lineattrv-$off
+    s\" \"rgb(255,0,0)\""   lineattrv-$!
+    s\" \"0.0\""            lineattrv-$!
+    s\" \"rgb(120,255,0)\"" lineattrv-$!
+    s\" \"1.0\""            lineattrv-$!
+    s\" \"2.0\""            lineattrv-$!
+;
+svg-attr#1
+
+: svg-attr#2 ( -- ) \ second set of attributes
     lineattrn-$off
     s" fill="           lineattrn-$!
     s" fill-opacity="   lineattrn-$!
@@ -36,9 +52,8 @@ list$: lineattrv  \ svg attribute values to work with a line but are paired with
     s\" \"1.0\""            lineattrv-$!
     s\" \"rgb(120,255,0)\"" lineattrv-$!
     s\" \"1.0\""            lineattrv-$!
-    s\" \"2.0\""            lineattrv-$!
-;
-init-svg-attributes
+    s\" \"3.0\""            lineattrv-$!
+;    
 
 list$: headern    \ header for svg .. normaly width and height 
 list$: headerv    \ header values that are paired with headern names
@@ -51,7 +66,7 @@ list$: headerv    \ header values that are paired with headern names
     headerv-$off
     s\" \"100\""         headerv-$!
     s\" \"100\""         headerv-$!
-    s\" \"0 0 100 100\"" headerv-$!
+    s\" \"0 0 100 100 \"" headerv-$!
 ;
 init-svg-header
 
@@ -75,14 +90,20 @@ variable svgoutput$  \ the primary output of the assembled svg string output
     loop
     s\" >\n" svgoutput$ $+! ;
 
-: svgmakepath ( -- ) \ will start path tag with lineattr.h1 attrabutes
-    s" <path " svgoutput$ $+!
+variable attribute$
+: svgattrout ( -- caddr u )
+    attribute$ init$
     lineattrv 2drop
     lineattrn swap drop 0 do
-	lineattrn-$@ svgoutput$ $+!
-	lineattrv-$@ svgoutput$ $+!
-	s"  " svgoutput$ $+!
-    loop
+	lineattrn-$@ attribute$ $+!
+	lineattrv-$@ attribute$ $+!
+	s"  " attribute$ $+!
+    loop attribute$ $@ ;
+
+: svgmakepath ( -- ) \ will start path tag with lineattr.h1 attrabutes
+    svg-attr#1
+    s" <path " svgoutput$ $+!
+    svgattrout svgoutput$ $+!
     s\" d=\" " svgoutput$ $+!
 ;
     
@@ -103,3 +124,76 @@ variable svgoutput$  \ the primary output of the assembled svg string output
     svgmakepath
     svgpathdata
     svgend ;
+
+variable circlejunk$
+list$: circlesvg$
+: makecirclefrompathdata ( -- )
+    circlesvg$-$off
+    svg-attr#2
+    svgattrout 2drop
+    pathdata$ swap drop 0 do
+	s\" <circle cx=\"" circlejunk$ $!
+	pathdata$-$@ 32 $split 2swap 2drop 32 $split 2swap circlejunk$ $+!
+	s\" \" cy=\"" circlejunk$ $+! circlejunk$ $+!
+	s\" \" r=\"4\"" circlejunk$ $+!
+	attribute$ $@ circlejunk$ $+!
+	s" />" circlejunk$ $+!
+	circlejunk$ $@ circlesvg$-$!
+    loop ;
+
+list$: localdata
+: findminmaxdata ( -- nmin nmax )
+    1000000 -1000000 { nmin nmax } 
+    localdata swap drop 0 do
+	localdata-$@ s>number? if d>s dup nmin min to nmin nmax max to nmax then
+    loop nmin nmax ;
+
+0 value mymin
+0 value mymax
+0 value xstep
+500 constant xmaxchart
+variable working$
+
+: makesvg2 ( ndata-index ndata-addr -- caddr u )
+    localdata->$!  
+    findminmaxdata to mymax to mymin
+    xmaxchart localdata swap drop / to xstep
+    \ make header size for svg
+    \ note need to add the border top and the text bottom size also later
+    headerv-$off
+    s\" \"" working$ $!
+    xmaxchart #to$ working$ $+!
+    s\" \"" working$ $+!
+    working$ $@ headerv-$!
+    s\" \"" working$ $!
+    mymax mymin - #to$ working$ $+!
+    s\" \"" working$ $+!
+    working$ $@ headerv-$!
+    s\" \"0 0 " working$ $!
+    xmaxchart #to$ working$ $+!
+    s"  " working$ $+!
+    mymax mymin - #to$ working$ $+!
+    s\" \"" working$ $+!
+    working$ $@ headerv-$!
+    \ recalculate data and form the path data statement for the ploted line
+    pathdata$-$off
+    localdata 2drop 
+    s" M 0 " working$ $!
+    localdata-$@ s>number? if d>s mymax swap - #to$ else 2drop s" 0" then
+    working$ $+! working$ $@ pathdata$-$!
+    localdata swap drop 1 localdata-$@ 2drop 
+    do
+	s" L " working$ $!
+	i xstep * #to$ working$ $+! s"  " working$ $+!
+	localdata-$@ s>number? if d>s mymax swap - #to$ working$ $+! working$ $@ pathdata$-$! else drop then 
+    loop
+    svgmakehead
+    svgmakepath
+    svgpathdata
+    \ draw circle from the path data just made
+    makecirclefrompathdata
+    circlesvg$ swap drop 0 do
+	circlesvg$-$@ svgoutput$ $+! 
+    loop
+    svgend
+;
