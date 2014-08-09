22a6073b0d14a4e9ca15ff184bc28179acc1ea92
harrypower <philipkingsmith@gmail.com>
2014-08-09 19:24:38 +0000

    changed names for words and data words added a new page for testing

---
+++ collection/svgmaker.fs
@@ -0,0 +1,324 @@
+\ This Gforth code is a Raspberry Pi Data logging code
+\    Copyright (C) 2014  Philip K. Smith
+
+\    This program is free software: you can redistribute it and/or modify
+\    it under the terms of the GNU General Public License as published by
+\    the Free Software Foundation, either version 3 of the License, or
+\    (at your option) any later version.
+
+\    This program is distributed in the hope that it will be useful,
+\    but WITHOUT ANY WARRANTY; without even the implied warranty of
+\    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\    GNU General Public License for more details.
+
+\    You should have received a copy of the GNU General Public License
+\    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\ This code has words to create svg code that can be used
+\ directly inside html 
+
+require gforth-misc-tools.fs
+require string.fs
+
+list$: svgattrn  \ svg attributes names to work with a line
+list$: svgattrv  \ svg attribute values to work with a line but are paired with linattrn names
+
+: svg-attr#1 ( -- ) \ initalizes svgattrn to default values
+    svgattrn-$off
+    s" fill="           svgattrn-$!
+    s" fill-opacity="   svgattrn-$!
+    s" stroke="         svgattrn-$!
+    s" stroke-opacity=" svgattrn-$!
+    s" stroke-width="   svgattrn-$!
+    svgattrv-$off
+    s\" \"rgb(255,0,0)\""   svgattrv-$!
+    s\" \"0.0\""            svgattrv-$!
+    s\" \"rgb(120,255,0)\"" svgattrv-$!
+    s\" \"1.0\""            svgattrv-$!
+    s\" \"2.0\""            svgattrv-$!
+;
+svg-attr#1
+
+: svg-attr#2 ( -- ) \ second set of attributes
+    svgattrn-$off
+    s" fill="           svgattrn-$!
+    s" fill-opacity="   svgattrn-$!
+    s" stroke="         svgattrn-$!
+    s" stroke-opacity=" svgattrn-$!
+    s" stroke-width="   svgattrn-$!
+    svgattrv-$off
+    s\" \"rgb(255,0,0)\""   svgattrv-$!
+    s\" \"1.0\""            svgattrv-$!
+    s\" \"rgb(120,255,0)\"" svgattrv-$!
+    s\" \"1.0\""            svgattrv-$!
+    s\" \"3.0\""            svgattrv-$!
+;
+
+: svg-attr#3 ( -- ) \ second set of attributes
+    svgattrn-$off
+    s" fill="           svgattrn-$!
+    s" fill-opacity="   svgattrn-$!
+    s" stroke="         svgattrn-$!
+    s" stroke-opacity=" svgattrn-$!
+    s" stroke-width="   svgattrn-$!
+    svgattrv-$off
+    s\" \"rgb(0,0,255)\""   svgattrv-$!
+    s\" \"0.0\""            svgattrv-$!
+    s\" \"rgb(0,100,200)\"" svgattrv-$!
+    s\" \"1.0\""            svgattrv-$!
+    s\" \"5.0\""            svgattrv-$!
+;
+
+: svg-attrtext ( -- ) \ basic text attributes
+    svgattrn-$off
+    s" fill="           svgattrn-$!
+    s" fill-opacity="   svgattrn-$!
+    s" stroke="         svgattrn-$!
+    s" stroke-opacity=" svgattrn-$!
+    s" stroke-width="   svgattrn-$!
+    s" font-size="      svgattrn-$!
+    svgattrv-$off
+    s\" \"rgb(0,0,255)\""   svgattrv-$!
+    s\" \"1.0\""            svgattrv-$!
+    s\" \"rgb(0,100,200)\"" svgattrv-$!
+    s\" \"0.0\""            svgattrv-$!
+    s\" \"2.0\""            svgattrv-$!
+    s\" \"20px\""           svgattrv-$!
+;
+
+list$: svgheadern    \ header for svg .. normaly width and height 
+list$: svgheaderv    \ header values that are paired with svgheadern names
+
+: init-svg-header ( -- ) \ default svg header
+    svgheadern-$off
+    s" width="       svgheadern-$!
+    s" height="      svgheadern-$!
+    s" viewBox="     svgheadern-$!
+    svgheaderv-$off
+    s\" \"100\""         svgheaderv-$!
+    s\" \"100\""         svgheaderv-$!
+    s\" \"0 0 100 100 \"" svgheaderv-$!
+;
+init-svg-header
+
+list$: svgdata$  \ the data values used in path... M,m,l,L and other path values 
+: init-test-data ( -- )
+    svgdata$-$off
+    s" M 10 30" svgdata$-$!
+    s" L 15 35" svgdata$-$!
+    s" L 27 40" svgdata$-$!
+    s" L 48 50" svgdata$-$!
+    s" L 97 20" svgdata$-$! ;
+
+variable svgoutput$  \ the primary output of the assembled svg string output
+
+: svgmakehead ( -- )  \ start with this word to make svg start tag
+    \ svgheadern and svgheaderv need to be setup before calling this word
+    svgoutput$ $off
+    s" <svg " svgoutput$ $!
+    svgheaderv 2drop 
+    svgheadern swap drop 0 do
+	svgheadern-$@ svgoutput$ $+!
+	svgheaderv-$@ svgoutput$ $+!
+	s"  " svgoutput$ $+!
+    loop
+    s\" >\n" svgoutput$ $+! ;
+
+variable svgattribute$
+: svgattrout ( -- caddr u )  \ this will take attributes from svgattrn and svgattrv string arrays
+    \ note the lineaatrn and svgattrv string arrays are paired so ensure proper matching in pairs
+    \ svgattribute$ can be used as an output of the last svgattrout command issued and is a string
+    svgattribute$ init$
+    svgattrv 2drop
+    svgattrn swap drop 0 do
+	svgattrn-$@ svgattribute$ $+!
+	svgattrv-$@ svgattribute$ $+!
+	s"  " svgattribute$ $+!
+    loop svgattribute$ $@ ;
+
+: svgmakepath ( -- ) \ will make path tag with svgattrn and svgattrv attributes
+    \ svgdata$ needs to be populated at call time for the path data
+    s" <path " svgoutput$ $+!
+    svgattrout svgoutput$ $+!
+    s\" d=\" " svgoutput$ $+!
+    svgdata$ swap drop 0 do
+	svgdata$-$@ svgoutput$ $+!
+	s"  " svgoutput$ $+!
+    loop
+    s\" \"> </path>\n" svgoutput$ $+! ;
+
+: svgmakecircle ( nx ny nr -- ) \ will make circle tage with svgattrn and svgattrv attributes
+    \ nx is cx in circle svg
+    \ ny is cy in circle svg
+    \ nr is r in circle svg
+    s\" <circle cx=\"" svgoutput$ $+!
+    rot #to$ svgoutput$ $+!
+    s\" \" cy=\"" svgoutput$ $+! swap #to$ svgoutput$ $+!
+    s\" \" r=\"" svgoutput$ $+! #to$ svgoutput$ $+!
+    s\" \" " svgoutput$ $+!
+    svgattrout svgoutput$ $+! 
+    s" />" svgoutput$ $+! ;
+
+bufr$: textbuff$
+: svgmaketext ( nx ny caddr u -- ) \ will start svg text tag and put lineattr attributes into it
+    \ nx is x possition
+    \ ny is y possition
+    \ caddr u is the counted string to place in the text 
+    textbuff$
+    s\" <text x=\"" svgoutput$ $+! 2swap swap #to$ svgoutput$ $+! s\" \" y=\"" svgoutput$ $+!
+    #to$ svgoutput$ $+! s\" \" " svgoutput$ $+!
+    svgattrout svgoutput$ $+! s" >" svgoutput$ $+! 
+    svgoutput$ $+! 
+    s" </text>" svgoutput$ $+! ;
+
+: svgend ( -- caddr u ) \ to finish the svg tag in the output string and deliver string
+    s" </svg>" svgoutput$ $+!
+    svgoutput$ $@ ;
+
+: make-a-pathsvg ( -- caddr u )  \ put all the parts together and output the final svg string
+    \ the attributes and path data need to be setup before calling
+    svgmakehead
+    svgmakepath
+    svgend ;
+
+\  ***** the stuff below here is to produce a chart using the above svg words ****
+list$: localdata
+
+variable mymin           \ will contain the chart data min absolute value
+0.0e mymin f!
+variable mymax           \ will contain the chart data max absolute value
+0.0e mymax f!
+variable xstep          \ how many x px absolute values to skip between data point plots
+0.0e xstep f!
+140 value xlablesize    \ the size taken up by the xlabel on the right side for chart
+1300 value xmaxchart    \ the max x absolute px of the chart .. change this value to make chart larger in x
+600 value ymaxchart     \ the max y absolute px of the chart .. change this value to make chart larger in y
+140 value ylablesize    \ the y label at bottom of chart size in absolute px
+70 value ytoplablesize  \ the y label at top of chart size in absolute px
+variable yscale          \ this is the scaling factor of the y values to be placed on chart
+0.0e yscale f!
+9 constant xminstep     \ the min distance in px between x ploted points 
+xmaxchart xminstep /
+value xmaxpoints        \ this will be the max allowed points to be placed on the chart 
+10 value xlableoffset   \ the offset to place lable from xlabelsize edge
+10 value ylableoffset   \ the offset to place lable from ( ymaxchart + ytoplablesize )
+10 value ylableqty      \ how many y lablelines and or text spots
+20 value ymarksize      \ the size of the y lable marks
+0 value ylabletxtpos    \ the offset of y lable text from svg window
+4 value circleradius    \ the radius of the circle used on charts for lines
+variable working$
+
+: f>s ( d: -- n ) ( f: r -- )
+    f>d d>s ;
+: s>f ( d: n -- ) ( f: -- r )
+    s>d d>f ;
+
+: makecirclefrompathdata ( -- ) \ makes circle data from the existing svgdata that was used to create chart lines
+    \ svgdata$ and svgattribute$ both need to be populated at call time
+    svgdata$ swap drop 0 do
+	svgdata$-$@ 32 $split 2swap 2drop 32 $split >float 
+	if
+	    >float if f>s f>s circleradius svgmakecircle else fdrop fdrop then   
+	else
+	    fdrop 2drop 
+	then
+    loop ;
+
+: findminmaxdata ( -- )  \ finds the min and max values of the localdata strings
+    \ note stored in mymax and mymin floating variables
+    localdata-$@ >float if fdup  mymax f! mymin f! then
+    localdata swap drop xmaxpoints min 0 do
+	localdata-$@ >float if fdup mymin f@ fmin mymin f! mymax f@ fmax mymax f! then
+    loop ;
+
+: svgchartheader ( -- ) \ will produce the svgheaderv string array stuff for the chart 
+    \ make header size for svg includes x and y lables 
+    svgheaderv-$off
+    s\" \"" working$ $!
+    xmaxchart xlablesize + #to$ working$ $+!
+    s\" \"" working$ $+!
+    working$ $@ svgheaderv-$!
+    s\" \"" working$ $!
+    ymaxchart ylablesize + ytoplablesize + #to$ working$ $+!
+    s\" \"" working$ $+!
+    working$ $@ svgheaderv-$!
+    s\" \"0 0 " working$ $!
+    xmaxchart xlablesize + #to$ working$ $+!
+    s"  " working$ $+!
+    ymaxchart ylablesize + ytoplablesize + #to$ working$ $+!
+    s\" \"" working$ $+!
+    working$ $@ svgheaderv-$! ;
+
+: svgchartmakepath ( -- ) \ will produce the svgdata$ string array stuff for the chart
+    \ recalculate data and form the path data statement for the ploted line
+    svgdata$-$off
+    localdata 2drop
+    s" M " working$ $! xlablesize #to$  working$ $+! s"  " working$ $+!
+    localdata-$@ >float \ s>number?
+    if
+	yscale f@ f* mymax f@ yscale f@ f* fswap f- f>s ytoplablesize + #to$
+    else
+	fdrop s" 0"
+    then
+    working$ $+! working$ $@ svgdata$-$!
+    localdata swap drop xmaxpoints min 1 localdata-$@ 2drop  
+    do
+	s" L " working$ $!
+	i s>f xstep f@ f* f>s xlablesize + #to$ working$ $+! s"  " working$ $+!
+	localdata-$@ >float \ s>number?
+	if
+	    yscale f@ f* mymax f@ yscale f@ f* fswap f- f>s ytoplablesize + #to$ working$ $+! working$ $@ svgdata$-$!
+	else
+	    fdrop
+	then
+    loop ;
+
+variable lableref$
+variable lablemark$
+: svgchartmakelables ( -- ) \ makes the lable lines for x and y on the chart
+    \ make the y lable line
+    svgdata$-$off
+    s" M " working$ $! xlablesize xlableoffset - #to$ working$ $+! s"  " working$ $+!
+    ytoplablesize #to$ working$ $+! s"  " working$ $+! working$ $@ 2dup lableref$ $! svgdata$-$!
+    s" L " working$ $! xlablesize xlableoffset - #to$ working$ $+! s"  " working$ $+!
+    ymaxchart ytoplablesize + ylableoffset + #to$ working$ $+! s"  " working$ $+! working$ $@ svgdata$-$!
+    \ make the x lable line
+    s" L " working$ $! xmaxchart xlablesize + #to$ working$ $+! s"  " working$ $+!
+    ymaxchart ytoplablesize + ylableoffset + #to$ working$ $+! s"  " working$ $+! working$ $@ svgdata$-$!
+    \ make y lable line marks
+    lableref$ $@ svgdata$-$!
+    s" l " working$ $! ymarksize -1 *  #to$ working$ $+! s"  " working$ $+! 0 #to$ working$ $+!
+    working$ $@ 2dup lablemark$ $! svgdata$-$!
+    ylableqty 1 do
+	lableref$ $@ svgdata$-$!
+	s" m " working$ $! 0 #to$ working$ $+! s"  " working$ $+! ymaxchart s>f ylableqty s>f f/ i s>f f* f>s #to$ working$ $+!
+	working$ $@ svgdata$-$!
+	lablemark$ $@ svgdata$-$!
+    loop
+    svgmakepath
+    \ make y lable text
+    svg-attrtext
+    ylableqty 0 do
+	ylabletxtpos ytoplablesize
+	ymaxchart s>f mymax f@ mymin f@ f- f/ mymax f@ mymin f@ f- ylableqty s>f f/ f* i s>f f* f>s + 
+	mymax f@ mymin f@ f- ylableqty s>f f/ i s>f f* mymax f@ fswap f- fto$ svgmaketext
+    loop ;
+
+: makesvgchart ( ndata-index ndata-addr -- caddr u )
+    localdata-$off
+    localdata->$!
+    xmaxchart xminstep / to xmaxpoints    
+    findminmaxdata 
+    xmaxchart s>f localdata swap drop xmaxpoints min s>f f/ xstep f!
+    ymaxchart s>f mymax f@ mymin f@ f- f/ yscale f!
+    svgchartheader  
+    svgchartmakepath 
+    svgmakehead
+    svg-attr#1     \ this is the line attribute 
+    svgmakepath
+    \ draw circle from the path data just made
+    svg-attr#2     \ this is the circle attribute 
+    makecirclefrompathdata
+    svg-attr#3     \ this is lable attribute 
+    svgchartmakelables
+    svgend ;
