d04816105621e40938906a723b41f952a8cf63e8
harrypower <philipkingsmith@gmail.com>
2015-03-09 19:43:26 +0000

    more work on get store sensor stuff

--- collection/getstoresensors.fs
+++ collection/getstoresensors.fs
@@ -17,10 +17,23 @@
 \  This code reads two sensors on i2c2 port and 2 analog sensors.
 \  The data is stored into a sqlite3 database.
 
+require gforth-misc-tools.fs
 require stringobj.fs
 require script.fs
+require ../BBB_Gforth_gpio/htu21d-object.fs
+require ../BBB_Gforth_gpio/bmp180-object.fs
 
 string heap-new constant shlast$
+string heap-new constant junk$
+
+next-exception @ constant gss-errorListStart
+s" co2 reading error!"                                      exception constant co2-err
+s" nh3 reading error!"                                      exception constant nh3-err
+s" Pressure sensor reading error!"                          exception constant press-err
+s" Humidity Temperature reading error!"                     exception constant ht-err
+s" Error place holder #1"                                   exception constant eholder1
+s" Error place holder #2"                                   exception constant eholder2
+next-exception @ constant gss-errorListEnd
 
 \ this next word is needed to prevent system defunct processes when using sh-get from script.fs
 : shgets ( caddr u -- caddr1 u1 nflag ) \ like shget but will not produce defunct processes
