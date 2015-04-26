15a22373d8b2a39af108270c312a0de0d8fda05f
harrypower <philipkingsmith@gmail.com>
2015-04-26 20:29:35 +0000

    removed stuff not needed or used

--- collection/get-nh3.js
+++
@@ -1,8 +0,0 @@
-var nh3 = require('/usr/local/lib/node_modules/bonescript');
-
-nh3.analogRead('P9_39', nh3print);
-
-function nh3print(x) {
-    console.log('nh3: ' + x.value);
-    console.log('err: ' + x.err);
-}
