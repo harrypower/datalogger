c7390e8ff22698e462c5ef0c78fea4cac37be8ce
harrypower <philipkingsmith@gmail.com>
2015-05-11 20:35:37 +0000

    test sending code now setup

---
+++ collection/testsend.fs
@@ -0,0 +1,48 @@
+require cryptobj.fs
+require gforth-misc-tools.fs
+require script.fs
+require stringobj.fs
+
+string heap-new constant shlast$
+string heap-new constant passf$
+string heap-new constant edata$
+string heap-new constant senddata$
+
+\ this next word is needed to prevent system defunct processes when using sh-get from script.fs
+: shgets ( caddr u -- caddr1 u1 nflag ) \ like shget but will not produce defunct processes
+    TRY   \ nflag is false the addr1 u1 is the result string from the sh command
+	\ nflag is true could mean memory was not allocated for this command or sh command failed
+	shlast$ !$
+	s"  ; echo ' ****'" shlast$ !+$ shlast$ @$ sh-get
+	2dup shlast$ !$
+	s\"  ****\x0a" search true =
+	if
+	    swap drop 6 =
+	    if
+		shlast$ @$ 6 - shlast$ !$ shlast$ @$
+	    else      
+		shlast	$ @$
+	    then
+	else
+	    shlast$ @$
+	then $?
+    RESTORE
+    ENDTRY ;
+
+path$ @$ encrypt_decrypt heap-new value myed
+
+s" stuff to send as test 12345"
+path$ @$ pass$ !$ s" testpassphrase" pass$ !+$
+pass$ @$ myed ' encrypt$ catch 0 = [if] edata$ !$ [else] ." encryption failed exiting now!" bye [then]
+
+s" curl --data '" senddata$ !$
+edata$ @$ senddata$ !+$
+s" ' 192.168.0.113:4445/testsend.shtml" senddata$ !+$
+senddata$ @$ shgets 0 =
+[if]
+    type
+[else]
+    ." some error in the curl statement occured!"
+[then]
+
+bye
