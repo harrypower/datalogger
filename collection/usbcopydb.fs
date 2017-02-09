9b1a2335ec060177cd75add54869dce71bc50c54
Philip Smith <philipkingsmith@gmail.com>
2017-02-09 16:12:43 -0700

    oops spelling in usb update

--- collection/usbcopydb.fs
+++ collection/usbcopydb.fs
@@ -39,70 +39,68 @@ variable dbbackupname$
 s" datalogged" dbbackupname$ $!
 
 : filetest ( caddr u -- nflag )  \ a file path and name is tested flag is true if file is present
-    s" test -e " junk$ $! junk$ $+! s"  && echo 'yes' || echo 'no'" junk$ $+! junk$ $@ shget throw s" yes" search swap drop swap drop
-    if true
-    else false
-    then ;
+  s" test -e " junk$ $! junk$ $+! s"  && echo 'yes' || echo 'no'" junk$ $+! junk$ $@ shget throw s" yes" search swap drop swap drop
+  if true
+  else false
+  then ;
 
 : checkusb? ( -- nflag ) \  nflag will be true a usb drive mounted and false for no drive mounted !
-    s" df -h " shget 0=
-    if
-	mount_name $@ search swap drop swap drop
-	if true
-	else false
-	then
-    else
-	2drop false
-    then ;
+  s" df -h " shget 0=
+  if
+  	mount_name$ $@ search swap drop swap drop
+  	if true
+  	else false
+  	then
+  else
+  	2drop false
+  then ;
 
 : finddev? ( -- nflag ) \ true returned means a usb drive to mount was found and mounted. A false returned means no mounted usb drive!
-    s" /dev/sda1" filetest
-    if  s" sudo mount /dev/sda1 " junk$ $! mount_name$ $@ junk$ $+! s"  -o nofail,sync,noexec " junk$ $+! junk$ $@ system $? 0=
+  s" /dev/sda1" filetest
+  if  s" sudo mount /dev/sda1 " junk$ $! mount_name$ $@ junk$ $+! s"  -o nofail,sync,noexec " junk$ $+! junk$ $@ system $? 0=
 	if
-	    checkusb?
-	    if  true
-	    else false
-	    then
-	else
-	    false
-	then
+    checkusb?
+    if  true
     else false
-    then ;
+    then
+	else
+    false
+	then
+  else false
+  then ;
 
 : umountdev ( -- nerror ) \ returns true only if the usb drive usb0 has been unmounted with no errors.  Returns false for any errors.
-    s" sudo umount " junk$ $! mount_name$ $@ junk$ $+! junk$ $@  system $? 0=
-    if  checkusb?
+  s" sudo umount " junk$ $! mount_name$ $@ junk$ $+! junk$ $@  system $? 0=
+  if  checkusb?
 	if  false  \ for some reason drive still there
 	else
-	    true   \ drive is unmounted.
+    true   \ drive is unmounted.
 	then
-    else  false
-    then ;
+  else  false
+  then ;
 
 : unmount_do ( -- ) \ trys to unmount the usb0 drive 3 times
-    3 0 ?do
-	umountdev true =
-	if leave then
-    loop ;
+  3 0 ?do
+    umountdev true =
+    if leave then
+  loop ;
 
 : copydb ( --  ) \ copies the db file onto the usb0 that should be mounted.
-    \ makes the file name have extention of yearmonthdayhour
-    s" sudo cp -u " junk$ $! db-path$ $@ junk$ $+! s"  " junk$ $+! mount_name$ $@ junk$ $+! s" /" junk$ $+!
-    dbbackupname$ $@ junk$ $+! s" ." junk$ $+!
-    time&date s>d dto$ junk$ $+! s>d dto$ junk$ $+! s>d dto$ junk$ $+! s>d dto$ junk$ $+! 2drop
-    junk$ $@ system ;
+  \ makes the file name have extention of yearmonthdayhour
+  s" sudo cp -u " junk$ $! db-path$ $@ junk$ $+! s"  " junk$ $+! mount_name$ $@ junk$ $+! s" /" junk$ $+!
+  dbbackupname$ $@ junk$ $+! s" ." junk$ $+!
+  time&date s>d dto$ junk$ $+! s>d dto$ junk$ $+! s>d dto$ junk$ $+! s>d dto$ junk$ $+! 2drop
+  junk$ $@ system ;
 
 
 : mount&copy ( -- ) \ trys to mount the usb drive then copy db file then unmount drive
-    checkusb? false =
-    if
-	finddev? drop
-    then
+  checkusb? false =
+  if
+  	finddev? drop
+  then
     checkusb? true =
-    if
-	copydb
-    then
-    \ unmount_do
-;
+  if
+    copydb
+  then ;
 
 \ mount&copy bye
